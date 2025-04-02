c1178d03bbda95e84d045bb870d5c11ff30f2879
atanwipro201 <aryanarmaan1912@gmail.com>
2025-04-02 23:18:07 +0100

    Day 3 add components or screens and buttons to test,btw the go back btn doesn't work

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Main from "./screens/main"
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Main />
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+      <StatusBar style="auto"/>
+      <Main/>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   }
